4b221f4b21443833ab498cff7db1ac20fed616c0
champix56 <desorbaix@free.fr>
2021-05-10 16:03:36 +0100

    class component evec state & lifecycle

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,31 @@
+import React from 'react';
 import './App.css';
 import Button from './components/Button/Button';
-function App() {
-  return (
-    <div className="App">
-     DEMAT BREIZH
-     <Button title="Mon boutton" bgcolor="tomato" ></Button>
-     <Button title="def. color"  ></Button>
-     <Button title="My button2" bgcolor="skyblue" style={{textDecoration:'underline',color:'grey'}} action={()=>{alert('Le boutton est cliqué')}}></Button>
-    </div>
-  );
-}
 
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state={text:"Hello", counter:0};
+  }
+  componentDidMount(){
+    this.setState({text:'je suis pret'});
+  }
+  com
+  componentDidUpdate(){
+    console.log('Le contenu du component a ete mis a jour',this.state);
+  }
+  render() {
+    return <div className="App">
+      <Button title="cliquez ICI !!" action={()=>{
+        //interdit de muter le state directement 
+        //this.state.counter=this.state.counter+1;
+        this.setState({counter:this.state.counter+1})
+        console.log(this.state);
+        
+      }} />
+      {JSON.stringify(this.state)}
+    </div>
+  }
+}
 export default App;
