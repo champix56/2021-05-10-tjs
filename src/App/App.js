4fbc1a664effb65ac0472c91d1b5b8088e206b1d
champix56 <desorbaix@free.fr>
2021-05-11 16:33:42 +0100

    subscribe by app.js

--- src/App/App.js
+++ src/App/App.js
@@ -3,45 +3,41 @@ import './App.css';
 import Button from './components/Button/Button';
 import AnimatedButton from './components/AnimatedButton/AnimatedButton';
 import MemeSVGViewer from './components/MemeSVGViewer/MemeSVGViewer';
-import {REST_SERVER_ADR} from './config/config'
+import { REST_SERVER_ADR } from './config/config'
 import store from './store/store'
 
 class App extends React.Component {
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
-    this.state={text:"Hello", counter:0,memes:[]};
+    this.state = { text: "Hello", counter: 0, memes: [] };
   }
-  componentDidMount(){
-    fetch(`${REST_SERVER_ADR}/memes?_expand=image`,{headers:{"Content-Type":"application/json"}})
-      .then((resp)=>resp.json(),(error)=>{console.log(error);return  [];})
-      .then(arr=>{
-        console.log(arr);
-        this.setState({memes:arr})
-        return arr;
-      })
+  componentDidMount() {
+    this.setState({ memes: store.getState().memes })
+    store.subscribe(() => {
+      this.setState({ memes: store.getState().memes })
+    })
   }
-  componentDidUpdate(){
-    console.log('Le contenu du component a ete mis a jour',this.state);
+  componentDidUpdate() {
+    console.log('Le contenu du component a ete mis a jour', this.state);
   }
   render() {
     return <div className="App">
-      <Button title="cliquez ICI !!" action={()=>{
+      <Button title="cliquez ICI !!" action={() => {
         //interdit de muter le state directement 
         //this.state.counter=this.state.counter+1;
-        this.setState({counter:this.state.counter+1})
+        this.setState({ counter: this.state.counter + 1 })
         console.log(this.state);
-        
+
       }} />
-      <br/>
-      <AnimatedButton title="Animated" action={()=>{console.log('hello');}}/>
-      <br/>
+      <br />
+      <AnimatedButton title="Animated" action={() => { console.log('hello'); }} />
+      <br />
       {
-        this.state.memes.map((element,index) => {
-          return <MemeSVGViewer meme={element} key={"viewer-"+index}/>
+        this.state.memes.map((element, index) => {
+          return <MemeSVGViewer meme={element} key={"viewer-" + index} />
         })}
-     
-      <br/>
+
+      <br />
 
       {JSON.stringify(this.state)}
     </div>
--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,50 +1,72 @@
-import {createStore} from 'redux';
+import { createStore } from 'redux';
+import { REST_SERVER_ADR } from '../config/config';
 /**
  * etat initial du state du store redux
  */
-const initialState={
-    memes:[],
-    images:[]
+const initialState = {
+    memes: [],
+    images: []
 }
 /**
  * actions public du reducer de store redux
  */
-export const MEMES_ACTIONS=Object.seal({ 
-    ADD_MEMES : 'ADD_MEMES' ,
-    ADD_MEME : 'ADD_MEME' ,
-    ADD_IMAGES : 'ADD_IMAGES' ,
-    ADD_IMAGE : 'ADD_IMAGE' 
+export const MEMES_ACTIONS = Object.seal({
+    ADD_MEMES: 'ADD_MEMES',
+    ADD_MEME: 'ADD_MEME',
+    ADD_IMAGES: 'ADD_IMAGES',
+    ADD_IMAGE: 'ADD_IMAGE'
 });
+const MEMES_PRIVATE_ACTIONS = Object.seal({
+    INIT: 'INIT'
+});
+
 /**
  * reducer du store redux
  * @param {object} state 
  * @param {object} action 
  */
-function memesReducer(state=initialState,action) {
-    switch(action.type)
-    {
-        case MEMES_ACTIONS.ADD_MEMES: return {...state,memes:[...state.memes,...action.values]}
-        case MEMES_ACTIONS.ADD_MEME: return {...state,memes:[...state.memes,action.value]}
-        case MEMES_ACTIONS.ADD_IMAGES: return {...state,images:[...state.images,...action.values]}
-        case MEMES_ACTIONS.ADD_IMAGE: return {...state,images:[...state.images,action.value]}
-        default:return state;
+function memesReducer(state = initialState, action) {
+    console.log(action.type);
+    const type = action.type.includes('@@redux/INIT') ? MEMES_PRIVATE_ACTIONS.INIT : action.type;
+    switch (type) {
+        case MEMES_PRIVATE_ACTIONS.INIT://phase d'init
+            fetch(`${REST_SERVER_ADR}/memes?_expand=image`, { headers: { "Content-Type": "application/json" } })
+                .then((resp) => resp.json(), (error) => { console.log(error); return []; })
+                .then(arr => {
+                    console.log(arr);
+                    store.dispatch({ type: MEMES_ACTIONS.ADD_MEMES, values: arr });
+                    return arr;
+                });
+                fetch(`${REST_SERVER_ADR}/images`, { headers: { "Content-Type": "application/json" } })
+                .then((resp) => resp.json(), (error) => { console.log(error); return []; })
+                .then(arr => {
+                    console.log(arr);
+                    store.dispatch({ type: MEMES_ACTIONS.ADD_IMAGES, values: arr });
+                    return arr;
+                });
+            return state;
+        case MEMES_ACTIONS.ADD_MEMES: return { ...state, memes: [...state.memes, ...action.values] }
+        case MEMES_ACTIONS.ADD_MEME: return { ...state, memes: [...state.memes, action.value] }
+        case MEMES_ACTIONS.ADD_IMAGES: return { ...state, images: [...state.images, ...action.values] }
+        case MEMES_ACTIONS.ADD_IMAGE: return { ...state, images: [...state.images, action.value] }
+        default: return state;
     }
 }
 /**
  * magasin partageable pour les composants
  */
-const store=createStore(memesReducer);
+const store = createStore(memesReducer);
 export default store;
 /**
  * souscription aux changements de l'etat du store
  */
-store.subscribe(()=>{
+store.subscribe(() => {
     console.log('le store a subit un changement');
     console.log(store.getState());
 });
 
-store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme1'}});
-store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme2'}});
-store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme3'}});
-store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme4'}});
-store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme5'}});
+// store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme1'}});
+// store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme2'}});
+// store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme3'}});
+// store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme4'}});
+// store.dispatch({type:MEMES_ACTIONS.ADD_MEME,value:{text:'meme5'}});
