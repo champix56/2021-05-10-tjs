06e6f426c307ed9496124e8d2c1baf3cbea1e504
champix56 <desorbaix@free.fr>
2021-05-12 11:02:51 +0100

    changement connexion au state du au combine reducer

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './App.css';
 import MemeSVGViewer from './components/MemeSVGViewer/MemeSVGViewer';
 import store, { initialState as storeInitialState } from './store/store'
-import MemeThumbnail from './components/MemeThumbnail/MemeThumbnail'
+import MemeThumbnail from './components/MemeThumbnail/MemeThumbnail';
+import MemeEditor from './components/MemeEditor/MemeEditor'
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { text: "Hello", counter: 0, ...storeInitialState };
   }
   componentDidMount() {
-    this.setState({ ...store.getState() })
+    this.setState({ ...store.getState().srvdata })
     // this.setState({ memes: store.getState().images })
     store.subscribe(() => {
-      this.setState({ ...store.getState() })
+      this.setState({ ...store.getState().srvdata })
       // this.setState({ images: store.getState().images })
     })
   }
@@ -21,6 +22,8 @@ class App extends React.Component {
   }
   render() {
     return <div className="App">
+      <MemeEditor/>
+      <hr/>
       <MemeThumbnail>
         {
           this.state.memes.map((element, index) => {
